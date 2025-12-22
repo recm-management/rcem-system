86b831d81f9072f4c4f69b0410e45d1a350b71b5
recm-management <recm.management@gmail.com>
2025-12-23 02:16:56 +0600

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,48 +1,31 @@
 <?php
 header("Content-Type: application/json");
 
-if (!isset($_FILES['photo'])) {
-  echo json_encode(["error" => "No file"]);
+if(!isset($_FILES['photo'])){
+  echo json_encode(["error"=>"No file"]);
   exit;
 }
 
-$dir = __DIR__ . "/uploads/staffs/";
-if (!is_dir($dir)) {
-  mkdir($dir, 0755, true);
-}
+$dir = __DIR__."/uploads/staffs/";
+if(!is_dir($dir)) mkdir($dir,0755,true);
 
-$tmp = $_FILES['photo']['tmp_name'];
-if (!$tmp) {
-  echo json_encode(["error" => "Upload failed"]);
-  exit;
-}
+$tmp=$_FILES['photo']['tmp_name'];
+$img=imagecreatefromstring(file_get_contents($tmp));
 
-$ext = strtolower(pathinfo($_FILES['photo']['name'], PATHINFO_EXTENSION));
-if (!in_array($ext, ["jpg", "jpeg", "png", "webp"])) {
-  echo json_encode(["error" => "Invalid format"]);
-  exit;
-}
+$w=imagesx($img); $h=imagesy($img);
+$max=600;
+$nw=$w>$h?$max:intval($w*$max/$h);
+$nh=$h>$w?$max:intval($h*$max/$w);
 
-$name = "staff_" . time() . "_" . rand(100,999) . ".jpg";
-$path = $dir . $name;
+$dst=imagecreatetruecolor($nw,$nh);
+imagecopyresampled($dst,$img,0,0,0,0,$nw,$nh,$w,$h);
 
-/* === Image resize & compress === */
-$src = imagecreatefromstring(file_get_contents($tmp));
-$w = imagesx($src);
-$h = imagesy($src);
+$name="staff_".time()."_".rand(100,999).".jpg";
+imagejpeg($dst,$dir.$name,70);
 
-$max = 600;
-$nw = $w > $h ? $max : intval($w * $max / $h);
-$nh = $h > $w ? $max : intval($h * $max / $w);
-
-$dst = imagecreatetruecolor($nw, $nh);
-imagecopyresampled($dst, $src, 0,0,0,0, $nw,$nh,$w,$h);
-imagejpeg($dst, $path, 70);
-
-imagedestroy($src);
-imagedestroy($dst);
+imagedestroy($img); imagedestroy($dst);
 
 echo json_encode([
-  "success" => true,
-  "path" => "uploads/staffs/".$name
+  "success"=>true,
+  "path"=>"uploads/staffs/".$name
 ]);
